a80bf7d1ad1efb741c889483cee2383a97d08100
Rodrigo Couto <r@rodg.co>
2018-10-11 08:49:35 -0300

    Day 6

---
+++ day06part2.rb
@@ -0,0 +1,16 @@
+puzzle = [4, 10, 4, 1, 8, 4, 9, 14, 5, 1, 14, 15, 0, 15, 3, 5]
+history = Array.new
+c = 0
+
+begin
+  history << puzzle.clone
+  n = puzzle.index(puzzle.max)
+  v = puzzle[n]
+  puzzle[n] = 0
+  (1..v).each do |x| 
+    puzzle[(n+x)%puzzle.length] = puzzle[(n+x)%puzzle.length] + 1
+  end
+  c += 1
+end until a = history.find {|p| p == puzzle }
+x = history.index(a)
+puts "#{c-x}"
